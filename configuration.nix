b673951d968d8ffabbe10bbec5d421ab9331405b
Mladen <root@brankovic.dev>
2024-01-09 21:15:45 +0100

    feat: wrap config in flake

--- configuration.nix
+++ configuration.nix
@@ -5,10 +5,12 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -79,7 +81,9 @@
       vim
       git
       vscode
-    #  thunderbird
+      rnix-lsp
+      nixpkgs-fmt
+      #  thunderbird
     ];
   };
 
@@ -88,10 +92,11 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      #  wget
+    ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+
+    packages.x86_64-linux =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        nixos-rebuild = subcommand: pkgs.writeScriptBin
+          "nixos-rebuild-${subcommand}"
+          "sudo nixos-rebuild ${subcommand} --flake ${./.}";
+      in
+      rec {
+        default = switch;
+
+        build = nixos-rebuild "build";
+        boot = nixos-rebuild "boot";
+        switch = nixos-rebuild "switch";
+
+        repl = nixos-rebuild "repl";
+        dry-build = nixos-rebuild "dry-build";
+      };
+  };
+}
