99f936838ba058f21777f2857af5c4b8205b2fcc
Mladen Branković <mladen.brankovic@golog.ch>
2024-01-22 16:04:02 +0100

    feat: update kitty

--- modules/users/kitty.nix
+++ modules/users/kitty.nix
@@ -15,13 +15,16 @@
     settings = {
       # https://sw.kovidgoyal.net/kitty/conf
 
+      background_opacity = "0.85";
+      background_blur = 40;
+
       cursor_shape = "underline";
       cursor_underline_thickness = 1;
       cursor_stop_blinking_after = 0;
 
       wheel_scroll_multiplier = font.size;
       remember_window_size = "no";
-      window_padding_width = font.size / 4;
+      window_padding_width = font.size / 3;
 
       modify_font = "underline_position 2";
 
