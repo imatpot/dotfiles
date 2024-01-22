2b26a1e2e34d6d2062a9732802c26bc8abd050d7
Mladen Branković <mladen.brankovic@golog.ch>
2024-01-22 14:50:18 +0100

    fix: apply secrets per host

--- users/mladen/config/secrets.nix
+++ users/mladen/config/secrets.nix
@@ -1,14 +1,16 @@
-{ config, inputs, outputs, ... }:
+{ config, inputs, outputs, system, hostname, ... }:
 
-{
-  sops.secrets = {
-    kubeConfigOnprem = outputs.lib.mkSecretFile {
-      source = "${inputs.vault}/kubernetes/config-onprem.crypt";
-      destination = "${config.home.homeDirectory}/.kube/config-onprem";
-    };
-    kubeConfigNatron = outputs.lib.mkSecretFile {
-      source = "${inputs.vault}/kubernetes/config-natron.crypt";
-      destination = "${config.home.homeDirectory}/.kube/config-natron";
+outputs.lib.mkFor system hostname {
+  hosts.mcdonalds = {
+    sops.secrets = {
+      kubeConfigOnprem = outputs.lib.mkSecretFile {
+        source = "${inputs.vault}/kubernetes/config-onprem.crypt";
+        destination = "${config.home.homeDirectory}/.kube/config-onprem";
+      };
+      kubeConfigNatron = outputs.lib.mkSecretFile {
+        source = "${inputs.vault}/kubernetes/config-natron.crypt";
+        destination = "${config.home.homeDirectory}/.kube/config-natron";
+      };
     };
   };
 }
