79e312d384dac1d0f97b6823cd88896568bfdc84
Mladen Branković <root@brankovic.dev>
2024-09-25 21:23:19 +0200

    chore(zen): bump

--- modules/users/zen-browser.nix
+++ modules/users/zen-browser.nix
@@ -1,7 +1,7 @@
 { inputs, outputs, config, system, ... }:
 
 let
-  version = "1.0.1-a.3";
+  version = "1.0.1-a.4";
   zen = inputs.zen-browser.packages.${system}.default;
 
   zen' = zen.overrideAttrs (oldAttrs: rec {
@@ -9,7 +9,7 @@ let
     src = builtins.fetchTarball {
       url =
         "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-      sha256 = "sha256:1xc30zr5zgfmnqajrppbq0mbzgd17lm3vld2l40xipmvkxqfcpi8";
+      sha256 = "sha256:0jjfr1201gfw0cy8q1jbr504994z33sbw8ip86c6xbww8qm60bqh";
     };
   });
 
