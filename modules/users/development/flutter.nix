0958f3ac32f86e52fff034fa8d77eadd10199601
Mladen Branković <mladen.brankovic@golog.ch>
2025-08-17 21:53:53 +0200

    feat: fvm from nixpkgs

--- modules/users/development/flutter.nix
+++ modules/users/development/flutter.nix
@@ -9,18 +9,9 @@
   config = outputs.lib.mkIf config.modules.users.dev.flutter.enable {
     modules.users.dev.adb.enable = true;
 
-    # Once https://github.com/leoafarias/fvm/pull/775 is merged, everything
-    # here could be replaced with a "lean" FVM installation.
-
-    # home.packages = with pkgs; [ unstable.flutter ];
-    programs = let
-      extra = ''
-        # Add FVM to PATH
-        export PATH="$HOME/.fvm_flutter/bin:$PATH"
-      '';
-    in {
-      bash.initExtra = extra;
-      zsh.initExtra = extra;
-    };
+    home.packages = with pkgs; [
+      master.fvm
+      master.fastlane
+    ];
   };
 }
