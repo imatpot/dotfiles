e92cca916cf4753b0c7da9e463d8685dcef45a95
Mladen Branković <root@brankovic.dev>
2025-08-24 02:27:36 +0200

    fix: enable on right option

--- modules/hosts/fail2ban.nix
+++ modules/hosts/fail2ban.nix
@@ -7,7 +7,7 @@
     modules.hosts.fail2ban.enable = outputs.lib.mkEnableOption "Enable fail2ban";
   };
 
-  config = outputs.lib.mkIf config.modules.hosts.nvidia.enable {
+  config = outputs.lib.mkIf config.modules.hosts.fail2ban.enable {
     services.fail2ban = outputs.lib.mkIf config.networking.firewall.enable {
       enable = true;
       ignoreIP = ["10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16"];
