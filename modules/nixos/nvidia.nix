73ff5ab1c2809de03395ab47d72f850c008414f5
Mladen Branković <root@brankovic.dev>
2025-08-31 17:58:54 +0200

    feat: cuda

--- modules/nixos/nvidia.nix
+++ modules/nixos/nvidia.nix
@@ -4,20 +4,56 @@
   pkgs,
   ...
 }:
-outputs.lib.mkConfigModule config false "nvidia"
+outputs.lib.mkModule config false "nvidia"
 {
-  hardware = {
-    nvidia.open = true;
+  cuda.enable = outputs.lib.mkDefaultEnableOption config.modules.nvidia.enable;
+}
+{
+  nixpkgs.config.cudaSupport = config.modules.nvidia.cuda.enable;
+  services.xserver.videoDrivers = ["nvidia"];
 
+  hardware = {
     graphics = {
       enable = true;
       enable32Bit = true;
+
+      extraPackages = with pkgs;
+        outputs.lib.optionals config.modules.nvidia.cuda.enable [
+          cudaPackages.cudatoolkit
+          cudaPackages.nccl
+
+          # NOTE: conflicting subpaths on 2025-09-01
+          (cudaPackages.cudnn.overrideAttrs (prev: {
+            postInstall =
+              (prev.postInstall or "")
+              + ''
+                rm $out/LICENSE
+              '';
+          }))
+        ];
     };
+
+    nvidia = {
+      open = true;
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+      nvidiaSettings = true;
+      modesetting.enable = true;
+
+      powerManagement = {
+        enable = false;
+        finegrained = false;
+      };
+    };
+
+    nvidia-container-toolkit.enable = true;
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
-
-  environment.systemPackages = with pkgs; [
-    nvtopPackages.full
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      nvtopPackages.full
+    ]
+    ++ outputs.lib.optionals config.modules.nvidia.cuda.enable [
+      cudaPackages.cudatoolkit
+    ];
 }
