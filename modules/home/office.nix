8a1b2f0bdda397dd9f2c8cc586ec63236f886345
Mladen Branković <root@brankovic.dev>
2025-09-14 19:05:57 +0200

    fix: onlyoffice theme

--- modules/home/office.nix
+++ modules/home/office.nix
@@ -7,5 +7,11 @@ outputs.lib.mkConfigModule config config.modules.gui.enable "office"
 {
   programs.onlyoffice = {
     enable = true;
+    settings =
+      # ini
+      ''
+        [General]
+        UITheme=theme-contrast-dark
+      '';
   };
 }
