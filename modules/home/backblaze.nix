67b6c7cf36402aca6511f9c613e729f5a38d5b0a
Mladen Branković <mladen.brankovic@golog.ch>
2025-09-18 17:04:09 +0200

    feat: b2 alias

--- modules/home/backblaze.nix
+++ modules/home/backblaze.nix
@@ -6,7 +6,11 @@
 }:
 outputs.lib.mkConfigModule config true "backblaze"
 {
-  home.packages = with pkgs; [
-    master.backblaze-b2
-  ];
+  home = {
+    shellAliases.b2 = "backblaze-b2";
+
+    packages = with pkgs; [
+      master.backblaze-b2
+    ];
+  };
 }
