9a28b8ff01307ad775544aefb3ad4660207a4788
Mladen Branković <root@brankovic.dev>
2025-09-27 14:50:36 +0200

    feat: add typst tools

--- modules/home/development/typst.nix
+++ modules/home/development/typst.nix
@@ -8,6 +8,9 @@ outputs.lib.mkConfigModule config false "dev.typst"
 {
   home.packages = with pkgs; [
     unstable.typst
+    typstyle
+    prettypst
+    tinymist
     pandoc
     texliveSmall
     poppler_utils
