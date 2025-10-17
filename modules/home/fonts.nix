c18ca2179795022549d60c16ff03e4ec3d432929
Mladen Branković <root@brankovic.dev>
2025-10-17 15:23:31 +0200

    feat: more fonts

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -14,11 +14,21 @@
     cascadia-code
     nerd-fonts.caskaydia-cove
     nerd-fonts.caskaydia-mono
+    departure-mono
+    nerd-fonts.departure-mono
+    cozette
+    scientifica
+    curie
   ];
 in
   outputs.lib.mkConfigModule config config.modules.gui.enable "fonts"
   {
     home.packages = fonts;
+
+    nixos.fonts = {
+      packages = fonts;
+      fontDir.enable = true;
+    };
 
     macos.fonts = {
       inherit fonts;
