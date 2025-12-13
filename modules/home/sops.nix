39a85bbb8c82fe081b84f63a36caebeaba33995c
Mladen Branković <root@brankovic.dev>
2025-12-13 21:24:52 +0100

    fix: include host secrets key

--- modules/home/sops.nix
+++ modules/home/sops.nix
@@ -8,4 +8,5 @@ outputs.lib.mkConfigModule config true "sops"
 {
   home.packages = with pkgs; [sops];
   sops.age.sshKeyPaths = ["${config.home.homeDirectory}/.ssh/aegis"];
+  nixos.sops.age.sshKeyPaths = ["/etc/ssh/aegis"];
 }
