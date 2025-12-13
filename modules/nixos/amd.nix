aaf6133eefccbfefea7df7775c53b6f569716c24
Mladen Branković <root@brankovic.dev>
2025-12-13 19:17:43 +0100

    feat: proper rocm support

--- modules/nixos/amd.nix
+++ modules/nixos/amd.nix
@@ -1,6 +1,7 @@
 {
   outputs,
   config,
+  pkgs,
   ...
 }:
 outputs.lib.mkConfigModule config false "amd"
@@ -13,11 +14,56 @@ outputs.lib.mkConfigModule config false "amd"
   };
 
   hardware = {
-    amdgpu.initrd.enable = true;
+    amdgpu = {
+      initrd.enable = true;
+      opencl.enable = true;
+    };
 
     graphics = {
       enable = true;
       enable32Bit = true;
     };
   };
+
+  systemd.tmpfiles.rules = let
+    rocmEnv = pkgs.symlinkJoin {
+      name = "rocm-combined";
+      paths = with pkgs.rocmPackages; [
+        rocblas
+        hipblas
+        clr
+        hipcc
+        rccl
+        miopen
+        miopen-hip
+        rocminfo
+        rocm-smi
+        rocm-runtime
+        rocm-device-libs
+      ];
+    };
+  in [
+    "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+  ];
+
+  environment.variables = {
+    PYTORCH_ROCM_ARCH = "gfx1030";
+    HSA_OVERRIDE_GFX_VERSION = "10.3.0y";
+    ROCM_PATH = "/opt/rocm";
+    HIP_VISIBLE_DEVICES = "0";
+  };
+
+  environment.systemPackages = with pkgs.rocmPackages; [
+    rocblas
+    hipblas
+    clr
+    hipcc
+    rccl
+    miopen
+    miopen-hip
+    rocminfo
+    rocm-smi
+    rocm-runtime
+    rocm-device-libs
+  ];
 }
