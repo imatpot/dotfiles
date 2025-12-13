c08ccadf7bfa887e894de8301c522320294e47a6
Mladen Branković <root@brankovic.dev>
2025-12-13 19:17:50 +0100

    feat: mamba

--- modules/home/development/python.nix
+++ modules/home/development/python.nix
@@ -8,6 +8,7 @@ outputs.lib.mkConfigModule config false "dev.python"
 {
   home = {
     packages = with pkgs; [
+      mamba-cpp
       (unstable.python3.withPackages (
         pythonPkgs:
           with pythonPkgs; [
@@ -20,6 +21,10 @@ outputs.lib.mkConfigModule config false "dev.python"
     shellAliases = rec {
       python = "python3";
       py = python;
+    };
+
+    sessionVariables = {
+      MAMBA_ROOT_PREFIX = config.home.homeDirectory + "/.mamba";
     };
   };
 }
