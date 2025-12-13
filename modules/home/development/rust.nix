e43d7feb5352b01f11cf656e42b0f32419c76de3
Mladen Branković <root@brankovic.dev>
2025-12-13 19:17:05 +0100

    feat: extend rust installation

--- modules/home/development/rust.nix
+++ modules/home/development/rust.nix
@@ -6,8 +6,23 @@
 }:
 outputs.lib.mkConfigModule config false "dev.rust"
 {
-  home.packages = with pkgs; [
-    cargo
-    rustc
-  ];
+  home = {
+    packages = with pkgs; [
+      cargo
+      rustc
+      rustfmt
+      clippy
+
+      gcc
+      stdenv.cc.cc.lib
+      libgcc.lib
+
+      openssl
+      pkg-config
+    ];
+
+    sessionVariables = {
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
 }
