65cb42826023f7cb83946ef0599ccaf75587d885
Mladen Branković <root@brankovic.dev>
2025-12-14 16:42:40 +0100

    feat: podman for linux by default

--- modules/home/virtualisation.nix
+++ modules/home/virtualisation.nix
@@ -1,9 +1,18 @@
 {
   outputs,
+  system,
+  hostname,
   config,
   ...
 }:
 outputs.lib.mkConfigModule config true "virtualisation"
+<| outputs.lib.mkFor system hostname
 {
-  nixos.modules.virtualisation.enable = true;
+  systems.linux = {
+    nixos.modules.virtualisation.enable = true;
+    services.podman = {
+      enable = true;
+      enableTypeChecks = true;
+    };
+  };
 }
