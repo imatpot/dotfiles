9c2447f319233ced324ae7cffb1804ae3980848e
Mladen Branković <mladen.brankovic@golog.ch>
2026-01-17 15:51:23 +0100

    chore: disable font management for macOS

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -1,12 +1,15 @@
 {
   outputs,
   config,
+  system,
   pkgs,
   ...
 }: let
   fonts = with pkgs; [
     # Writing
     libertine
+    libertinus
+    brill
     atkinson-hyperlegible-next
     noto-fonts
 
@@ -23,15 +26,10 @@
 in
   outputs.lib.mkConfigModule config config.modules.gui.enable "fonts"
   {
-    home.packages = fonts;
+    home.packages = outputs.lib.optionals (outputs.lib.isLinux system) fonts;
 
     nixos.fonts = {
       packages = fonts;
-      fontDir.enable = true;
-    };
-
-    macos.fonts = {
-      inherit fonts;
       fontDir.enable = true;
     };
   }
