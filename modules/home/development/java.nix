d44f00c8549de60f672174adcc9e4dc4f969b472
Mladen Branković <mladen.brankovic@golog.ch>
2026-08-17 13:44:26 +0200

    feat: configurable jdk version

--- modules/home/development/java.nix
+++ modules/home/development/java.nix
@@ -4,11 +4,15 @@
   pkgs,
   ...
 }:
-outputs.lib.mkConfigModule config false "dev.java" {
+outputs.lib.mkModule config false "dev.java"
+{
+  jdk = outputs.lib.mkDefaultEnableOption pkgs.unstable.jdk21;
+}
+{
   home.packages = with pkgs; [unstable.maven];
 
   programs.java = {
     enable = true;
-    package = pkgs.unstable.jdk21;
+    package = config.modules.dev.java.jdk;
   };
 }
