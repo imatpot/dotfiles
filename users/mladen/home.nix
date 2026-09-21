025eb4da5a2c32ea2ca7e3e33c2faa505cdc2ec6
Mladen Branković <mladen.brankovic@golog.ch>
2026-09-21 11:41:50 +0200

    fix: linux desktop stuff on linux only

--- users/mladen/home.nix
+++ users/mladen/home.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{outputs, lib, system, ...}: {
   programs.git.settings.user = {
     name = lib.mkDefault "Mladen Branković";
     email = lib.mkDefault "root@brankovic.dev";
@@ -8,7 +8,7 @@
     bitwarden.enable = lib.mkDefault true;
     stylix.enable = lib.mkDefault true;
 
-    niri.enable = lib.mkDefault true;
-    noctalia.enable = lib.mkDefault true;
+    niri.enable = lib.mkDefault <| outputs.lib.isLinux system;
+    noctalia.enable = lib.mkDefault <| outputs.lib.isLinux system;
   };
 }
